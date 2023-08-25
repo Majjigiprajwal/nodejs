33482cb88f6f6199827fa69901a441929558029c
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-08-25 19:23:32 +0530

    multiplyByArrowFunctions

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 console.log("Hello World from Nodejs");
+
+const multiply = (a,b)=>{
+    return a*b;
+}
+
+ const answer = multiply(5,10);
+
+ console.log(answer)
+
+ const student = {
+    name:'Rahul',
+    age:10,
+    rollNo:230,
+ }
