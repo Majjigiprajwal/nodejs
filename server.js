6db941a080cc5d13772dba655e12f8a0881599c3
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-08-26 11:03:39 +0530

    routes in http server

--- server.js
+++ server.js
@@ -5,7 +5,36 @@ const http = require('http');
 // }
 
  const server = http.createServer((req,res)=>{
-    console.log('prajwal')
+    const url = req.url;
+    if(url==='/'){
+        res.setHeader('Content-type','text/html');
+        res.write(`<html>`)
+        res.write(`<h1>Hello this is node js server</h1>`);
+        res.write(`</html>`)
+        res.end();
+    }
+   else if (url==='/node'){
+        res.setHeader('Content-type','text/html');
+        res.write(`<html>`)
+        res.write(`<h1>Welcome to MY Node js Project </h1>`);
+        res.write(`</html>`)
+        res.end();
+    }
+    else    if(url==='/home'){
+        res.setHeader('Content-type','text/html');
+        res.write(`<html>`)
+        res.write(`<h1>Welcome to Home Page </h1>`);
+        res.write(`</html>`)
+        res.end();
+    }
+    else if(url==='/about'){
+        res.setHeader('Content-type','text/html');
+        res.write(`<html>`)
+        res.write(`<h1>Welcome to About Page </h1>`);
+        res.write(`</html>`)
+        res.end();
+    }
+   
 });
 
 server.listen(4000)
